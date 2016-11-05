a3f3a231e585764b7374f1e89eaaf3d2a3534f35
David Zhu <hdavidzhu@gmail.com>
2016-11-05 13:01:29 -0400

    ADD: Jump test.

--- cpu.t.v
+++ cpu.t.v
@@ -6,16 +6,38 @@
 
 module testDavidsStuff ();
 
+  // INIT ======================================================================
+
+  wire [31:0] pc;
+
   reg clk;
+  reg [31:0] instruction;
 
   // DUT.
-  CPU dut ();
+  CPU dut (
+    .pc(pc),
+    .clk(clk),
+    .instruction(instruction)
+  );
+
+  // HELPERS ===================================================================
+
+  // Commands.
+  reg [5:0] CMD_JUMP = 6'd1;
+
+  reg        dutPassed;
+  reg [25:0] jumpTarget;
+
+  task completeInstructionCycle;
+    begin
+      // TODO: Update this time to the correct length of our instruction cycle.
+      #200;
+    end
+  endtask
 
   // Start the clock.
   initial clk = 1;
   always #1 clk = !clk;
-
-  reg dutPassed;
 
   initial begin
 
@@ -30,21 +52,26 @@ module testDavidsStuff ();
     // J =======================================================================
     // Jumps to the calculated address.
     // RTL:
-    //   PC = nPC;
-    //   nPC = (PC & 0xf0000000) | (target << 2);
+    //   PC = (PC & 0xf0000000) | (target << 2);
+
+    jumpTarget = 26'd203;
+    instruction = { CMD_JUMP, jumpTarget };
+    completeInstructionCycle();
+
+    if (pc !== {4'b0, 26'd203, 2'b0}) begin
+      dutPassed = 0;
+    end
 
     // JR ======================================================================
     // Jump to the address contained in register $s.
     // RTL:
-    //   PC = nPC;
-    //   nPC = $s;
+    //   PC = $s;
 
     // JAL =====================================================================
     // Jumps to the calculated address and stores the return address in $31.
     // RTL:
-    //   $31 = PC + 8 (or nPC + 4);
-    //   PC = nPC;
-    //   nPC = (PC & 0xf0000000) | (target << 2);
+    //   $31 = PC + 4;
+    //   PC = (PC & 0xf0000000) | (target << 2);
 
     // BNE =====================================================================
 
--- cpu.v
+++ cpu.v
@@ -1,2 +1,6 @@
-module CPU ();
+module CPU (
+  output [31:0] pc,
+  input         clk,
+  input  [31:0] instruction
+);
 endmodule
