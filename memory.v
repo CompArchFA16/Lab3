ac5cd58bcc0b86370dcc4e604ae34b6d01ee8e32
lisahachmann <lisa.hachmann@gmail.com>
2016-11-06 10:23:16 -0500

    Add files necessary for memory

--- memory.v
+++ memory.v
@@ -1,4 +1,4 @@
-module memory
+module Instr_memory
 (
   input clk, regWE,
   input[9:0] Addr,
@@ -14,7 +14,28 @@ module memory
     end
   end
 
-  initial $readmemh(“file.dat”, mem);
+  initial $readmemh(“Imem.dat”, mem);
+
+  assign DataOut = mem[Addr];
+endmodule
+
+module Data_memory
+(
+  input clk, regWE,
+  input[9:0] Addr,
+  input[31:0] DataIn,
+  output[31:0]  DataOut
+);
+
+  reg [31:0] mem[1023:0];
+
+  always @(posedge clk) begin
+    if (regWE) begin
+      mem[Addr] <= DataIn;
+    end
+  end
+
+  initial $readmemh(“Dmem.dat”, mem);
 
   assign DataOut = mem[Addr];
 endmodule
