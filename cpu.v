4d6f0e58eec5b86437afe503ddada170af9685ad
David Zhu <hdavidzhu@gmail.com>
2016-11-08 17:31:56 -0500

    ADD: Breakdown sections for pipeline circuits.

--- cpu.v
+++ cpu.v
@@ -1,8 +1,31 @@
 `include "opcodes.v"
+`include "gate_if_id.v"
 
 module CPU (
   output [31:0] pc,
   input         clk,
   input  [31:0] instruction
 );
+
+  // IF - Instruction Fetch ====================================================
+
+  wire [31:0] instruction_IF;
+  wire        pcPlus4_IF;
+
+  // ID - Instruction Decode ===================================================
+
+  wire [31:0] instruction_ID;
+  wire        pcPlus4_ID;
+
+  gate_IF_ID the_gate_If_ID (
+    .instruction_ID(instruction_ID),
+    .pcPlus4_ID(pcPlus4_ID),
+    .clk(clk),
+    .instruction_IF(instruction_IF),
+    .pcPlus4_IF(pcPlus4_IF)
+  );
+
+  // EX - Execute ==============================================================
+  // MEM - Data Memory =========================================================
+  // WB - Writeback ============================================================
 endmodule
