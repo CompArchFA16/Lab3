7979e837eecd394d7c98f858637c78b24eb4aa4f
lisahachmann <lisa.hachmann@gmail.com>
2016-11-09 20:45:23 -0500

    prep for merge

--- memory.v
+++ memory.v
@@ -14,28 +14,28 @@ module Instr_memory
     end
   end
 
-  initial $readmemh(“Imem.dat”, mem);
+  initial $readmemh("Imem2.dat", mem);
 
   assign DataOut = mem[Addr];
 endmodule
 
-module Data_memory
-(
-  input clk, regWE,
-  input[31:0] Addr,
-  input[31:0] DataIn,
-  output[31:0]  DataOut
-);
-
-  reg [31:0] mem[1023:0];
-
-  always @(posedge clk) begin
-    if (regWE) begin
-      mem[Addr] <= DataIn;
-    end
-  end
-
-  initial $readmemh(“Dmem.dat”, mem);
-
-  assign DataOut = mem[Addr];
-endmodule
+// module Data_memory
+// (
+//   input clk, regWE,
+//   input[31:0] Addr,
+//   input[31:0] DataIn,
+//   output[31:0]  DataOut
+// );
+//
+//   reg [31:0] mem[1023:0];
+//
+//   always @(posedge clk) begin
+//     if (regWE) begin
+//       mem[Addr] <= DataIn;
+//     end
+//   end
+//
+//   initial $readmemh("Dmem.dat", mem);
+//
+//   assign DataOut = mem[Addr];
+// endmodule
