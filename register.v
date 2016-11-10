98e24a075f42ea6d104a6f8316d9001abb0e263c
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-11-09 19:53:33 -0500

    changed addr to 5bit

--- register.v
+++ register.v
@@ -1,25 +1,25 @@
 module register
 
-#(parameter n = 32, parameter nreg = 10)
+#(parameter n = 32, parameter naddr = 5)
 
 (
 	input clk,
 
-	input [nreg-1:0] ra_addr,
-	input [nreg-1:0] rb_addr,
+	input [naddr-1:0] ra_addr,
+	input [naddr-1:0] rb_addr,
 
 	output [n-1:0] ra,
 	output [n-1:0] rb,
 
 	input wrEn, //RegWrite
-	input [nreg-1:0] wd_addr, //WriteRegister
+	input [naddr-1:0] wd_addr, //WriteRegister
 	input [n-1:0] wd //WriteData
 
 );
 
 // Register file storage
-reg [n-1:0] registers [nreg-1:0];
-reg [nreg-1:0] ra_reg, rb_reg;
+reg [n-1:0] registers [naddr-1:0];
+reg [naddr-1:0] ra_reg, rb_reg;
 
 always @(posedge clk) begin
     if (wrEn) begin
