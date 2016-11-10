ed1ea07242d52aa60df7c819df30ed865638e77f
lisahachmann <lisa.hachmann@gmail.com>
2016-11-10 11:52:21 -0500

    Decode module instantiated

--- instrdecode.v
+++ instrdecode.v
@@ -5,31 +5,38 @@
 module instrDecode
 (
     input clk,
-    input instrd,
+    input [31:0] instrd,
     input pcplus4d,
+    input [31:0] ResultW,
+    input [4:0] writeRegW,
+    input RegWriteW,
     output pcplus4e,
     output seImm,
-    output rte,
-    output rd3,
-    output rd1,
-    output rd2
+    output [4:0] rte,
+    output [4:0] rde,
+    output [31:0] rd1,
+    output [31:0] rd2
     );
 
     //fetch instr units
-    /*input [31:0] instr,
-    output [4:0] Op,
-    output [4:0] Funct,
-    output [4:0] A1,
-    output [4:0] A2,
-    output [4:0] RtE,
-    output [4:0] RdE,
-    output [15:0] Imm */
-    InstrFetchUnit fetch();
+    wire [5:0] op;
+    wire [5:0] funct;
+    wire [4:0] a1;
+    wire [4:0] a2;
+    wire [15:0] Imm;
+
+    InstrFetchUnit fetch(instrd, op, funct, a1, a2, rte, rde, Imm);
 
     //register file
-    /*[31:0]	ReadData1,[31:0]	ReadData2,[31:0]	WriteData, [4:0]	ReadRegister1,[4:0]	ReadRegister2,[4:0]	WriteRegister,RegWrite,Clk */
-    regfile reggie();
+    /*[31:0]ReadData1,[31:0]ReadData2,[31:0]WriteData, [4:0]ReadRegister1,[4:0]	ReadRegister2,[4:0]	WriteRegister,RegWrite,Clk */
+    wire [31:0] wd3;
+    wire [4:0] a3;
+    wire we3;
+    regfile reggie(rd1, rd2, ResultW, a1, a2, writeRegW, RegWriteW, clk);
+
     //sign extension
+    assign seImm = {Imm, 16'b0000000000000000}; //Imm is only 16 bits
     //control unit call
+
 
 endmodule
