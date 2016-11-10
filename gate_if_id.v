265004bf31a7b24a9bcc01975bf381d10c297fa0
David Zhu <hdavidzhu@gmail.com>
2016-11-09 21:05:02 -0500

    UPDATE: Gate to remove extra register.

--- gate_if_id.v
+++ gate_if_id.v
@@ -1,19 +1,12 @@
 module gate_IF_ID (
-  output [31:0] instruction_ID,
-  output        pcPlus4_ID,
-  input         clk,
-  input  [31:0] instruction_IF,
-  input         pcPlus4_IF
+  output reg [31:0] instruction_ID,
+  output reg        pcPlus4_ID,
+  input        clk,
+  input [31:0] instruction_IF,
+  input        pcPlus4_IF
 );
-
-  reg [31:0] instructionReg;
-  reg        pcPlus4Reg;
-
-  assign instruction_ID = instructionReg;
-  assign pcPlus4_ID     = pcPlus4Reg;
-
   always @ ( posedge clk ) begin
-    instructionReg <= instruction_IF;
-    pcPlus4Reg     <= pcPlus4_IF;
+    instruction_ID <= instruction_IF;
+    pcPlus4_ID     <= pcPlus4_IF;
   end
 endmodule
