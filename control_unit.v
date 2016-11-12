541389fcd702ffa41bdb953dcbd19d72a724dc1b
David Zhu <hdavidzhu@gmail.com>
2016-11-11 19:16:34 -0500

    ADD: Comments for control signals.

--- control_unit.v
+++ control_unit.v
@@ -35,16 +35,18 @@ module controlUnit (
       end
       `CMD_j: begin
         // TODO: David
+        // NOTE: Not even sure if these apply to J.
         regWrite_ID   <= 0;
         memToReg_ID   <= 0;
         memWrite_ID   <= 0;
-        branch_ID     <= 0;
+        branch_ID     <= 1;
         aluControl_ID <= 0;
         aluSrc_ID     <= 0;
         regDst_ID     <= 0;
       end
       `CMD_jr: begin
         // TODO: David
+        // NOTE: Again, I don't think these apply.
         regWrite_ID   <= 0;
         memToReg_ID   <= 0;
         memWrite_ID   <= 0;
@@ -55,7 +57,7 @@ module controlUnit (
       end
       `CMD_jal: begin
         // TODO: David
-        regWrite_ID   <= 0;
+        regWrite_ID   <= 1;
         memToReg_ID   <= 0;
         memWrite_ID   <= 0;
         branch_ID     <= 0;
@@ -92,11 +94,11 @@ module controlUnit (
       end
       `CMD_sub: begin
         // TODO: David
-        regWrite_ID   <= 0;
+        regWrite_ID   <= 1;
         memToReg_ID   <= 0;
         memWrite_ID   <= 0;
         branch_ID     <= 0;
-        aluControl_ID <= 0;
+        aluControl_ID <= 0; // TODO: Reference ALU commands.
         aluSrc_ID     <= 0;
         regDst_ID     <= 0;
       end
