1cb65bd8ce5ff5deb17ea95c632ab423e15a2b95
DHZBill <haozheng.du@students.olin.edu>
2016-11-12 14:31:56 -0500

    initializing memory

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,14 +1,20 @@
 module instructionmemory
 (
     input                       clk,
+    input 						writeEnable,
+    input [31:0]				dIn,
     input [31:0]                address,
-    output reg[31:0]               instruction,
+    output [31:0]               dOut
+
 );
     reg [31:0] memory [2**10-1:0];
 
     always @(posedge clk) begin
-        instruction <= memory[address];
+    	if(writeEnable)
+        	memory[address] <= dIn;
     end
-
+    
+    assign dOut = memory[address];
+    initial $readmemb("instructions.txt", memory);
 
 endmodule
