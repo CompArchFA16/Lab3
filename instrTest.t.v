214cf5f38821774ce1b3a4b4d18cd6c89c0b446a
Tom Heale <tah20112@gmail.com>
2016-11-12 10:58:25 -0500

    mem test

---
+++ instrTest.t.v
@@ -0,0 +1,31 @@
+`include "memory.v"
+
+module memory_test();
+    reg clk;
+    reg enable;
+    reg [31:0] addr;
+    reg [31:0] dataIn;
+    wire [31:0] dataOut;
+
+    Instr_memory tester (clk, enable, addr, dataIn, dataOut);
+
+    initial begin
+        $dumpfile("refer.vcd");
+        $dumpvars();
+        clk=0; enable=0; addr=32'h00000001; dataIn=32'h00000000;
+        #10 clk=1; #10 clk = 0;
+        $display("Data Out: %b Addr: %b", dataOut, addr);
+        clk=0; enable=0; addr=32'h0000000A; dataIn=32'h00000000;
+        #10 clk=1; #10 clk = 0;
+        $display("Data Out: %b Addr: %b", dataOut, addr);
+        clk=0; enable=0; addr=32'h0000000A; dataIn=32'h00000000;
+        #10 clk=1; #10 clk = 0;
+        $display("Data Out: %b Addr: %b", dataOut, addr);
+        clk=0; enable=1; addr=32'h0000000A; dataIn=32'hFFFFFFFF;
+        #10 clk=1; #10 clk = 0;
+        $display("Data Out: %b Addr: %b", dataOut, addr);
+        clk=0; enable=0; addr=32'h0000000A; dataIn=32'h00000000;
+        #10 clk=1; #10 clk = 0;
+        $display("Data Out: %b Addr: %b", dataOut, addr);
+    end
+endmodule
