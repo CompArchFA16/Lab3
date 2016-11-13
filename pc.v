7f1c6363348cb456b942d0e01bb0e84be20fb8f2
Anisha <Anisha>
2016-11-13 17:58:02 -0500

    added enable

--- pc.v
+++ pc.v
@@ -7,28 +7,56 @@
 module pc
 (
 input               clk,
+input				enable,
 output reg [31:0]   addr = 32'b00000000000000000000000000000000
 );
 
     always @(posedge clk) begin
-        if (addr == 32'b11111111111111111111111111111100) begin
-        end
-        else begin
-        addr <= addr + 4;
-        end
+    	if (enable) begin
+	        if (addr == 32'b11111111111111111111111111111100) begin
+	        end
+	        else begin
+	        addr <= addr + 4;
+	        end
+	    end
     end
 
 endmodule
 
 module quickpctest();
 reg clk;
+reg enable;
 wire [31:0] addr;
 
-pc peecee(clk, addr);
+pc peecee(clk, enable, addr);
 
 initial begin
 $display("Output: %b %b", clk, addr);
+clk = 1; enable = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; enable = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 1; enable = 0; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; enable = 0; #100
+$display("Output: %b %b", clk, addr);
 clk = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; #100
+$display("Output: %b %b", clk, addr);
+clk = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; enable = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; #100
+$display("Output: %b %b", clk, addr);
+clk = 1; #100
+$display("Output: %b %b", clk, addr);
+clk = 0; #100
+$display("Output: %b %b", clk, addr);
+clk = 1; enable = 0; #100
 $display("Output: %b %b", clk, addr);
 clk = 0; #100
 $display("Output: %b %b", clk, addr);
@@ -38,31 +66,7 @@ clk = 0; #100
 $display("Output: %b %b", clk, addr);
 clk = 1; #100
 $display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
-$display("Output: %b %b", clk, addr);
-clk = 1; #100
-$display("Output: %b %b", clk, addr);
-clk = 0; #100
+clk = 0; enable = 1; #100
 $display("Output: %b %b", clk, addr);
 clk = 1; #100
 $display("Output: %b %b", clk, addr);
