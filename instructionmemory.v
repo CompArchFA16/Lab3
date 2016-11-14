04a74595f7ca904cbb7fbaa9e2543d55bb2eeec1
jay-woo <jay.woo@students.olin.edu>
2016-11-13 22:28:30 -0500

    Adding assembly tests

--- instructionmemory.v
+++ instructionmemory.v
@@ -5,22 +5,12 @@
 //------------------------------------------------------------------------
 
 module instructionmemory
-#(
-    parameter len_instr = 32,
-    parameter num_instr = 32,
-    parameter len_addr = 5
-)
 (
-    input clk,
-    input[len_instr*num_instr-1:0] instructions,
-    input[len_addr-1:0] address,
-    output reg[len_instr-1:0] dataOut
+    input[9:0] address,
+    output[31:0] dataOut
 );
-    reg[len_instr*num_instr-1:0] memory;
-    reg[31:0] temp;
 
-    always @(posedge clk) begin
-      memory <= instructions;
-      dataOut <= memory[(address << 3) + 31 -: len_instr];
-    end
+    reg[31:0] memory[1023:0];
+    initial $readmemh("asmtest/jay_paul_tj/basic.dat", memory);
+    assign dataOut = memory[address];
 endmodule
