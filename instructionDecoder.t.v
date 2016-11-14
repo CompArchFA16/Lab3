f04ce511e67fa59991faf4b088ab252708a404b6
Ian Paul <ian.paul@students.olin.edu>
2016-11-14 01:50:54 +0000

    added shamt and funct to ID

--- instructionDecoder.t.v
+++ instructionDecoder.t.v
@@ -2,17 +2,17 @@
 module testInstructionDecoder();
 
 	reg[31:0] instruction;
-	wire [5:0] Opp;
-	wire [4:0] Rs, Rt, Rd;
+	wire [5:0] Opp, Funct;
+	wire [4:0] Rs, Rt, Rd, Shamt;
 	wire [15:0] Imm;
 	wire [25:0] Jadd;
 
-	instructionDecoder id0 (instruction, Opp, Rs, Rt, Rd, Imm, Jadd);
+	instructionDecoder id0 (instruction, Opp, Rs, Rt, Rd, Shamt, Funct, Imm, Jadd);
 
 	initial begin
-		$display("instruction| Opp| Rs| Rt| Rd| Imm| Jadd");
+		$display("instruction| Opp| Rs| Rt| Rd| Shamt| Funct| Imm| Jadd");
 		instruction = 32'b00000011111010101010111100000111; #1000;
-		$display("%b|%b|%b|%b|%b|%b|%b",instruction,Opp, Rs, Rt, Rd, Imm, Jadd);
+		$display("%b|%b|%b|%b|%b|%b|%b|%b|%b",instruction,Opp, Rs, Rt, Rd,Shamt, Funct, Imm, Jadd);
 
 	end
 	endmodule
--- instructionDecoder.v
+++ instructionDecoder.v
@@ -1,13 +1,13 @@
-module instructionDecoder (instruction, Opp, Rs, Rt, Rd, Imm, Jadd);
+module instructionDecoder (instruction, Opp, Rs, Rt, Rd, Shamt, Funct, Imm, Jadd);
 
 input[31:0] instruction;
-output[5:0] Opp;
-output[4:0] Rs, Rt, Rd;
+output[5:0] Opp, Funct;
+output[4:0] Rs, Rt, Rd, Shamt;
 output[15:0] Imm;
 output[25:0] Jadd;
 
 wire [31:0] instruction;
-wire [5:0] Opp;
+wire [5:0] Opp, Funct;
 wire [4:0] Rs, Rt, Rd;
 wire [15:0] Imm;
 wire [25:0] Jadd;
@@ -16,6 +16,8 @@ assign Opp[5:0] = instruction[31:26];
 assign Rs[4:0] = instruction[25:21];
 assign Rt[4:0] = instruction[20:16];
 assign Rd[4:0] = instruction[15:11];
+assign Shamt[4:0] = instruction[10:6];
+assign Funct[5:0] = instruction[5:0];
 assign Imm[15:0] = instruction[15:0];
 assign Jadd[25:0] = instruction[25:0];
 
