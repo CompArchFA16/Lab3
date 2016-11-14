1a0c3b737ad09a85d036004f90a12ae076664aff
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-11-13 23:24:40 -0500

    Working slope, TODO: handle negative numbers

--- asmtest/BDZ/slope.s
+++ asmtest/BDZ/slope.s
@@ -1,20 +1,6 @@
 # Slope of a line defined by two points: (x1, y1) and (x2, y2)
 # Slope is floored because the return type is int.
 
-# 	return num / den;
-
-# int main () {
-# 	int s = slope (0, 0, 3, 6);
-# 	printf("%d", s);
-# 	return s;
-# }
-
-# int slope (int x1, int y1, int x2, int y2) {
-# 	int num = y2 - y1;
-# 	int den = x2 - x1;
-# 	return num / den;
-# }
-
 main:
 	addi $a0, $zero, 0
 	addi $a1, $zero, 0
@@ -26,24 +12,28 @@ main:
 	sw $a1, 8($sp)
 	sw $a2, 4($sp)
 	sw $a3, 0($sp)
-	# set up $a0?
 	jal slope
 	lw $ra, 16($sp)
 	addi $sp, $sp, 20
 
 slope:
-	sub $t0, $a3, $a1 # numerator
-	sub $t1, $a2, $a0 # denominator
-	add $t2, $zero, $zero # sign
-	add $t3, $zero, $zero # division counter
-	addi $t4, $zero, 1 # division complete check
+	lw $t4, 12($sp) #a0
+	lw $t5, 8($sp) #a1
+	lw $t6, 4($sp) #a2
+	lw $t7, 0($sp) #a3
+
+	sub $t0, $t7, $t5 # numerator
+	sub $t1, $t6, $t4 # denominator
+
+	# TODO: logic for handling negative numbers
+
 	divloop:
 		sub $t0, $t0, $t1 # subtracting the den from the num
-		slt $t4, $zero, $t0 # check if 0 < $t0
-		bne $zero, $t4, endloop # branch if $t0 is not > 0 (<= 0)
+		slt $t4, $t0, $zero # check if num < 0
+		bne $zero, $t4, endloop # branch if num < 0
 		addi $t3, $t3, 1
 		j divloop
 	endloop:
 	j end
 
-end:
+end: # answer is stored in $t3
