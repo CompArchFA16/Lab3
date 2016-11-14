c591bea8dec00aa9d1d7158f9cbd6283abba498a
David Zhu <hdavidzhu@gmail.com>
2016-11-14 18:05:11 -0500

    UPDATE: Ram.

--- ram.v
+++ ram.v
@@ -1,20 +1,20 @@
-// Source: Lab 2 datamemory.v
-
 module RAM (
-  output reg [31:0] dataOut,
-  input             clk,
-  input      [31:0] address,
-  input             writeEnable,
-  input      [31:0] dataIn
+  output [31:0] dataOut,
+  input         clk,
+  input  [31:0] address,
+  input         writeEnable,
+  input  [31:0] dataIn
 );
 
   // For testing purposes, we don't need all that memory.
-  reg [31:0] memory [2**25 - 1:0];
+  reg [31:0] memory [(2**10)- 1:0];
 
+  assign dataOut = memory[address];
   always @(posedge clk) begin
-      if(writeEnable) begin
-          memory[address] <= dataIn;
-      end
-      dataOut <= memory[address];
+    if(writeEnable) begin
+      memory[address] <= dataIn;
+    end
   end
+
+  initial $readmemh("file.dat", memory);
 endmodule
