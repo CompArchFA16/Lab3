66a1652e002b2779476a75084a40f09726405573
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-11-14 11:09:07 -0500

    Slope program with .data section for parameters

--- asmtest/BDZ/slope.asm
+++ asmtest/BDZ/slope.asm
@@ -1,37 +1,44 @@
 # Slope of a line defined by two points: (x1, y1) and (x2, y2)
 # Slope is floored because the return type is int.
 
+.data # loading (x1, y1) and (x2, y2) values
+	x1: .word 0
+	y1: .word 0
+	x2: .word 3
+	y2: .word 6
+
+.text
 main:
-	addi $a0, $zero, 0
-	addi $a1, $zero, 0
-	addi $a2, $zero, 3
-	addi $a3, $zero, 6
+	# load address of parameters
+	la $a0, x1
+	la $a1, y1
+	la $a2, x2
+	la $a3, y2
+	
+	# loading contents at parameter addresses into volatile memory
+	lw $t4, ($a0)
+	lw $t5, ($a1)
+	lw $t6, ($a2)
+	lw $t7, ($a3)
+
+	# allocating memory for slope function
 	addi $sp, $sp, -20
 	sw $ra, 16($sp)
-	sw $a0, 12($sp)
-	sw $a1, 8($sp)
-	sw $a2, 4($sp)
-	sw $a3, 0($sp)
 	jal slope
 	lw $ra, 16($sp)
 	addi $sp, $sp, 20
 
 slope:
-	lw $t4, 12($sp) #a0
-	lw $t5, 8($sp) #a1
-	lw $t6, 4($sp) #a2
-	lw $t7, 0($sp) #a3
-
+	# calculating numerator and denominator
 	sub $t0, $t7, $t5 # numerator
 	sub $t1, $t6, $t4 # denominator
 
-	# TODO: logic for handling negative numbers
-
 	divloop:
+		# dividing numerator / denominator
 		sub $t0, $t0, $t1 # subtracting the den from the num
 		slt $t4, $t0, $zero # check if num < 0
 		bne $zero, $t4, endloop # branch if num < 0
-		addi $t3, $t3, 1
+		addi $t3, $t3, 1 # add to division counter
 		j divloop
 	endloop:
 	j end
