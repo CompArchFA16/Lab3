38bd61aeb9a4f5a4a58c66c8d91600e0f7438aa0
jay-woo <jay.woo@students.olin.edu>
2016-11-14 21:45:30 -0500

    Adding test for regfile

--- regfile.v
+++ regfile.v
@@ -67,3 +67,51 @@ input		Clk		// Clock (Positive Edge Triggered)
                                 RegOut[24], RegOut[25], RegOut[26], RegOut[27], RegOut[28], RegOut[29], RegOut[30], RegOut[31]);
 
 endmodule
+
+// Single-bit D Flip-Flop with enable
+//   Positive edge triggered
+module register
+(
+output reg	q,
+input		d,
+input		wrenable,
+input		clk
+);
+
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q = d;
+        end
+    end
+
+endmodule
+
+// 32-bit D Flip-Flop with enable
+//   Positive edge triggered
+module register32
+(
+output reg[31:0]	q,
+input[31:0]   		d,
+input wrenable,
+input	clk
+);
+
+    always @(posedge clk) begin
+        if(wrenable) begin
+            q = d;
+        end
+    end
+
+endmodule
+
+// 32-bit D Flip-Flop that always outputs zero
+module register32zero
+(
+output reg[31:0] q,
+input[31:0]   		d,
+input wrenable,
+input	clk
+);
+
+    initial q = 0;
+endmodule
