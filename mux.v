86a5c220ad6c94965f53b4e8e776d0ecd1f36ccf
shrutiyer <shruti.iyer@students.olin.edu>
2016-11-16 17:10:36 +0000

    ADD: Parametrized mux

--- mux.v
+++ mux.v
@@ -1,16 +1,28 @@
+// http://www.alteraforum.com/forum/showthread.php?t=22519
+
 `ifndef __MUX_V__
 `define __MUX_V__
 
-module mux
-#(parameter n=1, parameter m=2**n)
+module mux 
+#(   parameter WIDTH    = 8,
+     parameter CHANNELS = 4) 
 (
-	output out,
-	input [m-1:0] data,
-	input [n-1:0] sel
+    input   [(CHANNELS*WIDTH)-1:0]      in_bus,
+    input   [$clog2(CHANNELS-1)-1:0]    sel,   
+    output  [WIDTH-1:0]                 out
 );
 
-assign out = data[sel];
+genvar ig;
+    
+wire [WIDTH-1:0] input_array [0:CHANNELS-1];
 
+assign out = input_array[sel];
+
+generate
+    for(ig=0; ig<CHANNELS; ig=ig+1) begin: array
+        assign input_array[ig] = in_bus[(ig*WIDTH)+:WIDTH];
+    end
+endgenerate
 endmodule
 
 `endif
