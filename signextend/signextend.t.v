3020fbdeefc760e83649b67aa571115bd4edc2c6
DHZBill <haozheng.du@students.olin.edu>
2016-11-16 13:03:21 -0500

    signextend.t.v

--- signextend/signextend.t.v
+++ signextend/signextend.t.v
@@ -7,19 +7,45 @@ module testsignextend ();
   wire[31:0] signextended;
   reg[15:0] immediate;
   reg       issigned;
-
+  reg pass;
   signextend se1 (signextended, immediate, issigned);
 
   initial begin
-    $display("   Immediate   | issigned | signextended");
+    $dumpfile("signextended.vcd");
+    $dumpvars(0,testsignextend);
+    pass = 1;
+    $display("SignExtended Test:");
+
     immediate=16'hFFFF; issigned=1'b0; #1000
-    $display("%b |    %b     | %b", immediate, issigned, signextended);
+    if(signextended != 32'd65535) begin
+      $display("Test 1 Fail:");
+      $display("signextended: %b", signextended);
+    end
+    
     immediate=16'hFFFF; issigned=1'b1; #1000
-    $display("%b |    %b     | %b", immediate, issigned, signextended);
+    if(signextended != 32'b11111111111111111111111111111111) begin
+      $display("Test 2 Fail:");
+      $display("signextended: %b", signextended);
+    end
+
     immediate=16'h0000; issigned=1'b0; #1000
-    $display("%b |    %b     | %b", immediate, issigned, signextended);
+    if(signextended != 32'b0) begin
+      $display("Test 3 Fail:");
+      $display("signextended: %b", signextended);
+    end
+
     immediate=16'h0000; issigned=1'b1; #1000
-    $display("%b |    %b     | %b", immediate, issigned, signextended);
+    if(signextended != 32'b0) begin
+      $display("Test 4 Fail:");
+      $display("signextended: %b", signextended);
+    end
+    
+    if (pass == 1) begin
+            $display("SignExtended Tests Passed");
+        end
+        else begin
+            $display("SignExtended Tests Failed");
+        end
   end
 
 endmodule
