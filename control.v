9a24906bbca8293745fde4a2e4dfb5d68f6d1270
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-11-15 22:15:06 -0500

    updated

--- control.v
+++ control.v
@@ -3,114 +3,151 @@ module control
 	input clk,
 	input [5:0] instruction, //instruction[31:26]
 
-	output reg RegDst, //Mux for Register_WriteRegister_in
+	output reg [1:0] RegDst, //Mux for Register_WriteRegister_in
 	output reg Branch, //AND with ALU_Zero_out to get PCSrc
 	output reg MemRead, //read data from data memory
-	output reg MemtoReg, //Mux for selecting between DataMemory_ReadData_out
-	output reg [1:0] ALUOp, //for ALU control
+	output reg [1:0] MemtoReg, //Mux for selecting between DataMemory_ReadData_out
+	output reg [2:0] ALUOp, //for ALU control
 	output reg MemWrite, //write data to data memory
 	output reg ALUSrc, //goes to ALU control
-	output reg RegWrite 
+	output reg RegWrite,
+	output reg [1:0] Jump
 );
 
 initial begin
-	RegDst <= 0;
-	RegWrite <=0;
-	ALUSrc <=0;
-	MemWrite <=0;
-	MemRead <=0;
-	MemtoReg <=0;
-	Branch <=0;
-	ALUOp[1] <=0;
-	ALUOp[0] <=0;
+	RegDst <= 2'b00;
+	RegWrite <= 0;
+	ALUSrc <= 0;
+	MemWrite <= 0;
+	MemRead <= 0;
+	MemtoReg <= 2'b00;
+	Branch <= 0;
+	Jump <= 2'b00;
+	ALUOp <= 3'b000;
 end
 
 always @* begin
 
 	case(instruction)
 
-		6'b000000: begin //jr, add, sub, slt
-			RegDst <=0;
-			RegWrite <=0;
-			ALUSrc <=0;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=1;
-			ALUOp[1] <=0;
-			ALUOp[0] <=0;
+		6'b100000: begin //add
+			RegDst <= 2'b00;
+			RegWrite <= 1; //check
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b001;
 		end
 
-		6'b000010: begin //j 
-			RegDst <=0;
-			RegWrite <=0;
-			ALUSrc <=0;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=1;
-			ALUOp[1] <=0;
-			ALUOp[0] <=0;
-		end
-
-		6'b000011: begin //jal
-			RegDst <=0;
-			RegWrite <=1;
-			ALUSrc <=0;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=1;
-			ALUOp[1] <=0;
-			ALUOp[0] <=0;
-		end
-
-		6'b000101: begin //bne
-			RegDst <=0;
-			RegWrite <=0;
-			ALUSrc <=0;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=1;
-			ALUOp[1] <=0;
-			ALUOp[0] <=1;
+		6'b100010: begin //sub
+			RegDst <= 2'b00;
+			RegWrite <= 1; //check
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b010;
 		end
 
 		6'b001110: begin //xori
-			RegDst <=0;
-			RegWrite <=1;
-			ALUSrc <=1;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=0;
-			ALUOp[1] <=1;
-			ALUOp[0] <=0;
+			RegDst <= 2'b01;
+			RegWrite <= 1;
+			ALUSrc <= 1;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b100;
+		end
+
+		6'b101010: begin //slt
+			RegDst <= 2'b00;
+			RegWrite <= 1;
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b011;
+		end
+
+		6'b000101: begin //bne
+			RegDst <= 2'b00;
+			RegWrite <= 0;
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 1;
+			Jump <= 2'b00;
+			ALUOp <= 3'b010;
+		end
+
+		6'b000010: begin //j
+			RegDst <= 2'b00;
+			RegWrite <= 0;
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b01;
+			ALUOp <= 3'b000;
+		end
+
+		6'b000011: begin //jal
+			RegDst <= 2'b10;
+			RegWrite <= 1;
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b10;
+			Branch <= 0;
+			Jump <= 2'b01;
+			ALUOp <= 3'b000;
+		end
+
+		6'b001000: begin //jr
+			RegDst <= 2'b00;
+			RegWrite <= 0;
+			ALUSrc <= 0;
+			MemWrite <= 0;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b10; //diff from j and jal
+			ALUOp <= 3'b000;
 		end
 
 		6'b100011: begin //lw
-			RegDst <=0;
-			RegWrite <=1;
-			ALUSrc <=1;
-			MemWrite <=0;
-			MemRead <=1;
-			MemtoReg <=1;
-			Branch <=0;
-			ALUOp[1] <=0;
-			ALUOp[0] <=0;
+			RegDst <= 2'b00;
+			RegWrite <= 1;
+			ALUSrc <= 1;
+			MemWrite <= 0;
+			MemRead <= 1;
+			MemtoReg <= 2'b01;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b001;
 		end
 
 		6'b101011: begin //sw
-			RegDst <=0;
-			RegWrite <=1;
-			ALUSrc <=1;
-			MemWrite <=0;
-			MemRead <=0;
-			MemtoReg <=0;
-			Branch <=0;
-			ALUOp[1] <=0;
-			ALUOp[0] <=0;
+			RegDst <= 2'b00;
+			RegWrite <= 0;
+			ALUSrc <= 1;
+			MemWrite <= 1;
+			MemRead <= 0;
+			MemtoReg <= 2'b00;
+			Branch <= 0;
+			Jump <= 2'b00;
+			ALUOp <= 3'b001;
 		end
 
 		default: begin
--- cpu.v
+++ cpu.v
@@ -10,7 +10,7 @@
 `timescale 1ns / 1ps
 
 // Two-input MUX 
-module mux #(parameter W = 32)
+module mux2 #(parameter W = 32)
 (
     input[W-1:0]    in0,
     input[W-1:0]    in1,
@@ -18,6 +18,25 @@ module mux #(parameter W = 32)
     output[W-1:0]   out
 );
     assign out = (sel) ? in1 : in0;
+endmodule
+
+module mux3 #(parameter W = 32)
+(
+    input[W-1:0]    in0,
+    input[W-1:0]    in1,
+    input[W-1:0]	in2,
+    input [1:0]     sel,
+    output reg [W-1:0]   out
+);
+    always @ (sel or in0 or in1 or in2)
+	begin : MUX
+	 case(sel) 
+	    2'b00 : out = in0;
+	    2'b01 : out = in1;
+	    2'b10 : out = in2;
+	    default: out = 0;
+	 endcase 
+	end
 endmodule
 
 
@@ -48,17 +67,19 @@ module cpu
 );
 
 	wire [31:0] pc_in;
+	wire [31:0] branch_out;
 	wire [31:0] pc_out;
 	wire [31:0] inst;
 
-	wire RegDst; //Mux for Register_WriteRegister_in
+	wire [1:0] RegDst; //Mux for Register_WriteRegister_in
 	wire Branch; //AND with ALU_Zero_out to get PCSrc
 	wire MemRead; //read data from data memory
-	wire MemtoReg; //Mux for selecting between DataMemory_ReadData_out
-	wire [1:0] ALUop; //for ALU control
+	wire [1:0] MemtoReg; //Mux for selecting between DataMemory_ReadData_out
+	wire [2:0] ALUop; //for ALU control
 	wire MemWrite; //write data to data memory
 	wire ALUsrc; //goes to ALU control
 	wire RegWrite;
+	wire [1:0] Jump;
 
 	wire [4:0] writeRegister;
 	wire [31:0] writeData;
@@ -68,7 +89,6 @@ module cpu
 
 	wire [31:0] SEinst;
 	wire [31:0] ALU_in;
-	wire [2:0] ALUcontrol;
 	wire [31:0] ALUresult;
 	wire ALUzero;
 
@@ -77,20 +97,82 @@ module cpu
 	wire [31:0] addALUres;
 	//wire PCSrc;
 
-	pc cpuPC(.clk(clk), .pc_in(pc_in), .pc_out(pc_out));
-	instMem cpuIM(.PC(pc_out), .instruction(inst));
+	pc cpuPC(.clk(clk), 
+			 .pc_in(pc_in), 
+			 .pc_out(pc_out));
+
+	instMem cpuIM(.read_address(pc_out), 
+				  .instruction(inst));
+
 	control cpuControl(.instruction(inst[31:26]),
-					   .RegDst(RegDst), .Branch(Branch), .MemRead(MemRead), .MemtoReg(MemtoReg), .ALUOp(ALUop), .MemWrite(MemWrite), .ALUSrc(ALUsrc), .RegWrite(RegWrite));
-	mux #(5) mux5_inst_reg(.in0(inst[20:16]), .in1(insr[15:11]), .sel(RegDst), .out(writeRegister));
-	register cpuRegister(.clk(clk), .ra_addr(inst[25:21]), .rb_addr(instr[20:16]), .wrEn(RegWrite), .wd_addr(writeRegister), .wd(writeData), .ra(reg_readData1), .rb(reg_readData2));
-	signExtend cpuSE(.seIn(inst[15:0]), .seOut(SEinst));
-	mux mux_reg_alu(.in0(reg_readData2), .in1(SEinst), .sel(ALUsrc), .out(ALU_in));
-	alucontrol cpualucontrol(.ALUop(ALUop), .instruction(inst[5:0]), .ALUcontrol(ALUcontrol));
-	alu cpuALU(.alucontrol(ALUcontrol), .a(reg_readData1), .b(ALU_in), .aluRes(ALUresult), .zero(ALUzero));
-	datamemory cpu_dm(.clk(clk), .readData(datamem_readData), .address(ALUresult), .MemWrite(MemWrite), .MemRead(MemRead), .writeData(reg_readData2));
-	mux mux_datamem(.in0(ALUresult), .in1(datamem_readData), .sel(MemtoReg), .out(writeData));
-	add_pc cpu_add_pc(.in(pc_out), .out(pc4_out));
-	add_alu cpu_add_alu(.in0(pc4_out),.in1(SEinst), .out(addALUres));
-	mux mux_pc(.in0(pc4_out), .in1(addALUres), .sel(Branch & ALUzero), .out(pc_in));
+					   .RegDst(RegDst), 
+					   .Branch(Branch),
+					   .Jump(Jump),
+					   .MemRead(MemRead), 
+					   .MemtoReg(MemtoReg), 
+					   .ALUOp(ALUop), 
+					   .MemWrite(MemWrite), 
+					   .ALUSrc(ALUsrc), 
+					   .RegWrite(RegWrite));
+
+	mux3 #(5) mux5_inst_reg(.in0(inst[20:16]), 
+		                   .in1(inst[15:11]), 
+		                   .in2(5'b11111), //check if this is reg[$31]
+		                   .sel(RegDst), 
+		                   .out(writeRegister));
+
+	register cpuRegister(.clk(clk), 
+						 .ra_addr(inst[25:21]), 
+						 .rb_addr(inst[20:16]), 
+						 .wrEn(RegWrite), 
+						 .wd_addr(writeRegister), 
+						 .wd(writeData), 
+						 .ra(reg_readData1), 
+						 .rb(reg_readData2));
+
+	signExtend cpuSE(.seIn(inst[15:0]), 
+		             .seOut(SEinst));
+
+	mux2 mux_reg_alu(.in0(reg_readData2), 
+		            .in1(SEinst), 
+		            .sel(ALUsrc), 
+		            .out(ALU_in));
+
+	alu cpuALU(.alucontrol(ALUop), 
+		       .a(reg_readData1), 
+		       .b(ALU_in), 
+		       .aluRes(ALUresult), 
+		       .zero(ALUzero));
+
+	datamemory cpu_dm(.clk(clk), 
+		              .readData(datamem_readData), 
+		              .address(ALUresult), 
+		              .MemWrite(MemWrite), 
+		              .MemRead(MemRead), 
+		              .writeData(reg_readData2));
+
+	add_pc cpu_add_pc(.in(pc_out), 
+		              .out(pc4_out));
+
+	mux3 mux_datamem(.in0(ALUresult), 
+		             .in1(datamem_readData), 
+		             .in2(pc4_out),
+		             .sel(MemtoReg), 
+		             .out(writeData));
+
+	add_alu cpu_add_alu(.in0(pc4_out),
+		                .in1(SEinst), 
+		                .out(addALUres));
+
+	mux2 mux_branch(.in0(pc4_out), 
+		            .in1(addALUres), 
+		            .sel(Branch & !ALUzero), 
+		            .out(branch_out));
+
+	mux3 mux_jump(.in0(branch_out), 
+		          .in1({pc4_out[31:28], inst[25:0], 2'b00}), 
+		          .in2(reg_readData1),
+		          .sel(Jump), 
+		          .out(pc_in));
 
 endmodule
