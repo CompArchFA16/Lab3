6128d5bd7c7190b3d5f9fd784779ea6ebdfe446b
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-11-15 22:20:54 -0500

    not working instMem for instructors

--- instMem.v
+++ instMem.v
@@ -1,16 +1,16 @@
 module instMem
 (
-    input [31:0]            read_address,
+    input [31:0]        read_address,
     output [31:0]       instruction
 );
     reg [31:0] instructionMem [31:0];
 
-    //initial $readmemh(“assembly_test.dat”, instructionMem);
+    initial $readmemh(“assembly_test.dat”, instructionMem);
     //initial $readmemh(“test.dat”, instructionMem);
 
-    initial begin
-        instructionMem[0] <= 32'h2008000a;
-    end
+    // initial begin
+    //     instructionMem[0] <= 32'h2008000a;
+    // end
 
     assign instruction = instructionMem[read_address];
 
