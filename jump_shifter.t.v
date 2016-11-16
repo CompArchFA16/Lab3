266e10d3a2121fdaf9d1f503f459d02e799aeba0
tj-kim <taejin.kim96@gmail.com>
2016-11-16 15:31:26 -0500

    jump shifter test bench finalized and edited

--- jump_shifter.t.v
+++ jump_shifter.t.v
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------
-// Shift Register test bench
+// Jump shifter test bench
 //------------------------------------------------------------------------
 `timescale 1ns / 1ps
 
@@ -12,29 +12,51 @@ module testjump_shifter();
     reg[3:0]        pc_bits;
     wire[31:0]      extended_imm;
 
+    reg             dutpassed;
     
     // Instantiate with parameter width = 8
-    jump_shifter dut(immediate, pc_bits,extended_imm);
+    jump_shifter dut(extended_imm, immediate, pc_bits);
     always begin
     #1 clk = ~clk;
     end
 
+
     initial begin
+
+        dutpassed = 1;
 
         clk=1; 
         pc_bits = 4'b0101;
         immediate = 26'b11111111111111111111111111; #0
 
+        if(extended_imm != 32'b01011111111111111111111111111100) begin
+            dutpassed = 0;  // Set to 'false' on failure
+            $display("Test Case 1 Failed: MSB in imm 1 implementation failed");
+        end
+        else begin
+            $display("Test Case 1 Passed");
+        end
+        #2
 
-        $display("  immediate               |     Ext      ");
-        $display("    %b      |   %b   ",  immediate, extended_imm);
+
+        // $display("  immediate               |     Ext      ");
+        // $display("    %b      |   %b   ",  immediate, extended_imm);
  
-        immediate = 26'b01111111111111111111111111; #0
+        immediate = 26'b01111111111111111111111111; #2
+        if(extended_imm != 32'b01010111111111111111111111111100) begin
+            dutpassed = 0;  // Set to 'false' on failure
+            $display("Test Case 2 Failed: MSB in imm 0 implementation failed");
+        end
+        else begin
+            $display("Test Case 2 Passed");
+        end
 
 
-        $display("  immediate               |     Ext      ");
-        $display("    %b      |   %b   ",  immediate, extended_imm);       
+        // $display("  immediate               |     Ext      ");
+        // $display("    %b      |   %b   ",  immediate, extended_imm);       
 
+
+        $display("dutpassed: %b",  dutpassed);
         #2 $finish;
     end
 
--- jump_shifter.v
+++ jump_shifter.v
@@ -8,9 +8,9 @@
 
 module jump_shifter
 (
-    output [31:0]                 extended_imm
+    output [31:0]                 extended_imm,
     input  [25:0]                 immediate,
-    input  [3:0]				  pc_bits,
+    input  [3:0]				  pc_bits
 );
 
    assign extended_imm = { pc_bits, immediate, {2'b00}};
