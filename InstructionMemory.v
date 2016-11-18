7d163a8c1da4b63bfcf94e2891561241e9c8038b
yycho0108 <jchocholate@gmail.com>
2016-11-18 02:09:41 +0000

    fixed most of errors

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -4,18 +4,11 @@ module instructionMemory
 (
     input                       clk,
     input 						writeEnable,
-    input [31:0]				dIn,
     input [31:0]                address,
     output [31:0]               dOut
 
 );
     reg [31:0] memory [2**10-1:0];
-
-    always @(posedge clk) begin
-    	if(writeEnable)
-        	memory[address] <= dIn;
-    end
-    
     initial $readmemb("instructions.txt", memory);
     assign dOut = memory[address];
 
