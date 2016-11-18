f306bd230ec46103dc6e5c0e5a18c31ceee0d237
kghite <kghite@gmail.com>
2016-11-18 01:41:09 -0500

    Finished: signextender and test

--- submodules/signextender.v
+++ submodules/signextender.v
@@ -10,13 +10,10 @@ module signextender (
     	if (imm[15] === 1'b0) begin // If the first bit is zero, add 16 zeros
       		signextendOut <= { 16'b0000000000000000, imm };
     	end
-    	if (imm[15] === 1'b1) begin
-    		signextendOut <= { 16'b1111111111111111, imm };
-    	end
 
-    	// else begin // Otherwise add 16 ones
-      	//	signextendOut <= { 16'b1, imm };
-    	// end
+    	else begin // Otherwise add 16 ones
+      		signextendOut <= { 16'b1111111111111111, imm };
+    	end
 	end
 
 endmodule
--- tests/signextender.t.v
+++ tests/signextender.t.v
@@ -9,6 +9,9 @@ module testSignextend ();
 	// Output
 	wire [31:0] signextendOut;
 
+	// Indicates whether register file passed tests
+	reg dutpassed;
+
 	signextender signext(clk, imm, signextendOut);
 
 	initial clk = 0;
@@ -16,21 +19,38 @@ module testSignextend ();
 	initial begin
 
 		clk = 1;
+		dutpassed = 1;
 
-        $display("-------------------------");
-    	$display("SIGNEXTENDER TEST CASES");
-		$display("-------------------------");
-
+        //$display("-------------------------");
+    	//$display("SIGNEXTENDER TEST CASES");
+		//$display("-------------------------");
 
         // Sign extend with ones
-        $display("| imm | signextendOut |");
-    	imm = 16'b1010101010101010; #1000000
-		$display("| 	%b 		| 		%b 		| ", imm, signextendOut);
+        //$display("| imm | signextendOut |");
+    	imm = 16'b1010101010101010; #100
+		//$display("| 	%b 		| 		%b 		| ", imm, signextendOut);
+
+    	// Verify test result
+    	if(signextendOut != 32'b11111111111111111010101010101010) begin
+      		dutpassed = 0;
+    	end 
 
         // Sign extend with zeros
-        $display("| imm | signextendOut |");
+        //$display("| imm | signextendOut |");
     	imm = 16'b0101010101010101; #100
-		$display("| 	%b 		| 		%b 		| ", imm, signextendOut);
+		//$display("| 	%b 		| 		%b 		| ", imm, signextendOut);
+
+    	// Verify test result
+    	if(signextendOut != 32'b00000000000000000101010101010101) begin
+      		dutpassed = 0;
+    	end 
+
+    	if (dutpassed != 0) begin
+    		$display("SNE: PASSED");
+    	end
+    	else begin
+    		$display("SNE: FAILED");
+    	end
 
 		$finish;
 
