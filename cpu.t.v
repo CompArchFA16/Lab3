50cb04f3848a0fb9897ada04b791e243fcc00461
shrutiyer <shruti.iyer@students.olin.edu>
2016-11-18 01:38:23 +0000

    ADD: cpu testbench

---
+++ cpu.t.v
@@ -0,0 +1,24 @@
+`include "cpu.v"
+module testcpu();
+
+  reg clk;
+  reg pass;
+
+  cpu cputest(clk);
+
+  initial 
+  begin
+    clk=0;
+  end
+
+  always 
+    #5 clk=!clk;
+
+  initial begin
+    $dumpfile("cpu.vcd");
+    $dumpvars(0, cputest);
+
+    #5000
+  end
+
+endmodule
