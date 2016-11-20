5c8bde740107eaf371449c0696b919278e2adba3
David Zhu <hdavidzhu@gmail.com>
2016-11-20 14:45:13 -0500

    ADD: JAL handling for the jump unit.

--- jump_unit.v
+++ jump_unit.v
@@ -8,6 +8,17 @@ module jump_unit (
   input  [31:0] instruction_ID
 );
   wire [31:0] pc_checkpoint_j;
-  assign pc_checkpoint_j = instruction_IF[31:26] === `CMD_j ? (instruction_IF[25:0] << 2) : pc_original;
-  assign pc_out = instruction_ID[31:26] === `CMD_jr ? pc_from_regfile : pc_checkpoint_j;
+  wire [31:0] instruction_ID;
+
+  // Handle J.
+  assign pc_checkpoint_j = instruction_IF[31:26] === `CMD_j ?
+                             (instruction_IF[25:0] << 2) :
+                             pc_original;
+
+  // Handle JAL and JR.
+  assign pc_out = instruction_ID[31:26] === `CMD_jal ?
+                     pc_original + 8 :
+                     instruction_ID[31:26] === `CMD_jr ?
+                       pc_from_regfile :
+                       pc_checkpoint_j;
 endmodule
