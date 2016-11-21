cd9e6ae9058374cffe245e2f2a6290e1eba4989c
David Zhu <hdavidzhu@gmail.com>
2016-11-21 17:24:55 -0500

    UPDATE: Return on assembly program.

--- asmtest/BDZ/slope.asm
+++ asmtest/BDZ/slope.asm
@@ -9,17 +9,23 @@ main:
 	addi $a2, $zero, 3
 	addi $a3, $zero, 6
 
+  # Move the stack pointer.
 	addi $sp, $sp, -20
 	sw $ra, 16($sp)
 
+  # Store the arguments into the stack.
 	sw $a0, 12($sp)
 	sw $a1, 8($sp)
 	sw $a2, 4($sp)
 	sw $a3, 0($sp)
 
+  # Call our slope method.
 	jal slope
-	lw $ra, 16($sp)
+
+  # Return and restore the stack.
+  lw $ra, 16($sp)
 	addi $sp, $sp, 20
+  j end
 
 slope:
 	# loading contents at parameter addresses into volatile memory
@@ -37,11 +43,12 @@ slope:
 		sub $t0, $t0, $t1 # subtracting the den from the num
 		slt $t4, $t0, $zero # check if num < 0
 		bne $zero, $t4, endloop # branch if num < 0
-		addi $t3, $t3, 1 # add to division counter
+		addi $v0, $v0, 1 # add to division counter
 		j divloop
 	endloop:
-	j end
+	jr $ra
 
-end: # answer is stored in $t3
+end: # answer is stored in $v0
+  syscall
 
 .data
