c41d91a289777c98a8408ddc0d35005296211a2d
kghite <kghite@gmail.com>
2016-11-28 19:09:22 -0500

    CPU module definitions skeleton

--- processor.v
+++ processor.v
@@ -25,13 +25,17 @@ wire RegDstD;
 wire [15:0] signExtendOut;
 
 // ALU 
-wire [2:0] muxIndexA;
-wire invertBA;
-wire cinA;
-wire invertResA;
+wire [31:0] resA;
+wire coutA;
+wire oflA;
+wire zeroA;
 
 // Datamemory
 wire dataOutM;
+
+// Registerfile
+wire [31:0] readData1R;
+wire [31:0] readData2R;
 
 ///// Modules /////
 
@@ -39,6 +43,10 @@ wire dataOutM;
 signextender(clk, IMM CONNECTION, signExtendOut);
 
 // ALU
-
+ALU(resA, coutA, oflA, zeroA, A CONNECTION, B CONNECTION, CMD CONNECTION);
 
 // Controller
+
+
+// Registerfile
+registerfile(readData1R, readData2R, WRITEDATA CONNECTION, READREG1 CONNECTION, READREG2 CONNECTION, WRITEREG CONNECTION, REGWRITE CONNECTION, clk);
